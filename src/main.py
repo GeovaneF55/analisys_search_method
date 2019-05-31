5d782b54d0aa1576c8c330ee64c6eb826f0f52b3
GeovaneF55 <geovanefss@gmail.com>
2019-05-31 19:15:12 -0300

    Algoritmos BFS e Guloso

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
-from algorithms import bfs
+from algorithms import bfs, greedy, astar
+
+# Print puzzle of the given board
+def print_puzzle(label, board):
+    print(label)
+    for row in board:
+        print(row)
+    print('\n')
 
 if __name__ == '__main__':
     solution = [[' ', '1', '2'], ['3', '4', '5'], ['6', '7', '8']]
     test     = [['1', '4', '2'], ['3', '5', '8'], ['6', ' ', '7']]
+    # test    = [['5', '2', '8'], ['4', '1', '7'], [' ', '3', '6']]
 
-    nmoves = bfs(test, solution)
-    print(nmoves)
+    print_puzzle('Solução:', solution)
+    print_puzzle('Teste:', test)
+
+    print('ALGORITMO BFS - Qtde passos: ', bfs(test, solution))
+
+    print('ALGORITMO GULOSO - Qtde passos: ', greedy(test, solution))
+
+    print('ALGORITMO A ESTRELA (está igual ao guloso) - Qtde passos: ', astar(test, solution))
+
